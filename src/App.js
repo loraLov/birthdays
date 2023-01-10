176dc7220ef3949d3566c0c462c70ed4a342766d
loraLov <lauzhynskaya@gmail.com>
2023-01-10 18:02:42 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import { useState } from "react";
+import {data} from './data';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  const [person, setPerson] = useState(data);
+
+  const removePerson = (id) => {
+    let newData = person.filter (item => item.id!==id);
+    setPerson(newData);
+  }
+
+
+
+return(
+  <div>
+<div className = "container">
+      <h1>Birthday List</h1>
+    </div>
+    <div className="container">
+      <h2>{person.length} Birthdays left</h2>
+    </div>
+{person.map((element =>{
+  const {id, image, name, date, day} = element;
+  return(
+    <div key={id}> 
+    
+    <div className="container">
+    <div className = "main">
+      <img src = {image} width="350px" alt = "person"/>
+      <h4> {id}. {name}</h4>
+      <p>{date}</p>
+      <p>{day}</p>
+       <button className = "btn" onClick ={() => removePerson(id)}> Remove</button>
+     </div>
+     </div>
     </div>
   );
+ 
+}))}
+ <div className = "container">
+    <button className="btnBig" onClick = {() => setPerson([])}>Delete all</button>
+  </div>
+</div>
+);
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,82 @@
+export const data = [
+    {
+        id:1,
+        image:"https://images.pexels.com/photos/831012/pexels-photo-831012.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+        name:"Jane",
+        date: "January 13",
+        day: "Friday"
+    },
+    {
+        id:2,
+        image:"https://images.pexels.com/photos/1914877/pexels-photo-1914877.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+        name:"Kate",
+        date: "January 27",
+        day: "Friday"
+    },
+    {   id:3,
+        image:"https://images.pexels.com/photos/842811/pexels-photo-842811.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+        name:"Jack",
+        date: "February 7",
+        day: "Tuesday"
+    },
+    {   id:4,
+        image:"https://images.pexels.com/photos/1102343/pexels-photo-1102343.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+        name:"Hellen",
+        date: "March 18",
+        day: "Saturday"
+    },
+
+    {   id:5,
+        image:"https://images.pexels.com/photos/1090387/pexels-photo-1090387.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+        name:"Iris",
+        date: "April 3",
+        day: "Monday"
+    },
+    {   id:6,
+        image:"https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+        name:"Max",
+        date: "April 19",
+        day: "Wednesday"
+    },
+
+
+    {   id:7,
+        image:"https://images.pexels.com/photos/1036623/pexels-photo-1036623.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+        name:"Laura",
+        date: "May 17",
+        day: "Wednesday"
+    },
+    {   id:8,
+        image:"https://images.pexels.com/photos/413885/pexels-photo-413885.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+        name:"Katrin",
+        date: "June 14",
+        day: "Wednesday"
+    },
+    {   id:9,
+        image:"https://images.pexels.com/photos/1250643/pexels-photo-1250643.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+        name:"Mary",
+        date: "August 13",
+        day: "Sunday"
+    },
+    {   id:10,
+        image:"https://images.pexels.com/photos/842569/pexels-photo-842569.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+        name:"Alex",
+        date: "September 4",
+        day: "Monday"
+    },
+    {   id:11,
+        image:"https://images.pexels.com/photos/846741/pexels-photo-846741.jpeg",
+        name:"Matthew",
+        date: "October 15",
+        day: "Sunday"
+    },
+
+    {   id:12,
+        image:"https://images.pexels.com/photos/13866673/pexels-photo-13866673.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+        name:"Daniel",
+        date: "December 6",
+        day: "Wednesday"
+    }
+
+
+]
